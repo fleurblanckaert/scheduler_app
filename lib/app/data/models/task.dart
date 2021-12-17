daaaac53885e7b22ebebf3f06ab39d30fa1ff575
Raje Singh <raje.singh@dcmail.ca>
2021-12-17 18:45:20 -0500

    add task model

---
+++ lib/app/data/models/task.dart
@@ -0,0 +1,50 @@
+class Task {
+
+  /// database id 
+  int? id; 
+  String? title;
+  String? note;
+  int? isCompleted;
+  String? date;
+  String? startTime;
+  String? endTime;
+  int? color;
+
+  Task({
+    required this.id,
+    required this.title,
+    required this.note,
+    required this.isCompleted,
+    required this.date,
+    required this.startTime,
+    required this.endTime,
+    required this.color
+  });
+
+  Task.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    title = json['title'];
+    note = json['note'];
+    isCompleted = json['isCompleted'];
+    date = json['date'];
+    startTime = json['startTime'];
+    endTime = json['endTime'];
+    color = json['color'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+
+    data['id'] = id;
+    data['title'] = title;
+    data['note'] = note;
+    data['isCompleted'] = isCompleted;
+    data['startTime'] = startTime;
+    data['endTime'] = endTime;
+    data['color'] = color;
+    data['date'] = date;
+
+    return data;
+  }
+
+}
